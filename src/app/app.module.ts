06dbb894eb56ac4adeb80203d8b5d44ec56d9ce5
MaheshB77 <mbansode7696@gmail.com>
2021-05-01 21:12:10 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AmexioWidgetModule } from "amexio-ng-extensions"; // Import Amexio library
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmexioWidgetModule
   ],
   providers: [],
   bootstrap: [AppComponent]
